eb89720d48c835b786ecf8165d3d41cd7ed736de
IITII <ccmejx@gmail.com>
2020-02-09 01:09:30 +0800

    deal with v2ray.sh options

--- startUp.sh
+++ startUp.sh
@@ -9,12 +9,19 @@
 if [ -e /var/log/v2ray_install.log ]; then
     exit 0
 fi
-
+# Modify at here
 declare INSTALL_MODE=""
 declare SITE_NAME=""
 declare WS_PATH=""
 declare UUID=""
 declare DDNS_KEY=""
+# Don't modify the following lines if \
+# you are not very clearly know what you are doing
+declare _INSTALL_MODE="-t $INSTALL_MODE"
+declare _SITE_NAME="-w $SITE_NAME"
+declare _WS_PATH="-p $WS_PATH"
+declare _UUID="-u $UUID"
+declare _DDNS_KEY="--ddns $DDNS_KEY"
 
 declare release="ubuntu"
 
@@ -64,8 +71,23 @@ check_command() {
 }
 main() {
     git clone https://github.com/iitii/autov2ray /root/AutoV2ray
+    if [ -z $INSTALL_MODE ]; then
+        _INSTALL_MODE=""
+    fi
+    if [ -z $SITE_NAME ]; then
+        _SITE_NAME=""
+    fi
+    if [ -z $DDNS_KEY ]; then
+        _DDNS_KEY=""
+    fi
+    if [ -z $WS_PATH ]; then
+        _WS_PATH=""
+    fi
+    if [ -z $UUID ]; then
+        _UUID=""
+    fi
     cd /root/AutoV2ray \
-        && bash v2ray.sh -t $INSTALL_MODE -w $SITE_NAME --ddns $DDNS_KEY -p $WS_PATH -u $UUID \
+        && bash v2ray.sh $_INSTALL_MODE $_SITE_NAME $_DDNS_KEY $_WS_PATH $_UUID \
          >> /var/log/v2ray_install.log
 }
 
